e72aeb8c07965ae58b9fb6b2cf7fb5edf97c8c8e
seonhyeokjun <tjsrmatn@naver.com>
2024-08-02 13:46:21 +0900

    Eureka Service Discovery - 프로젝트 생성

---
+++ src/test/java/com/example/discoveryService/EcoomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.discoveryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoomerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
